20848730585a2e73343f29e9de7de64286393ccd
Ihor Shylo <igor_shilo@ukr.net>
2022-09-16 11:37:22 +0300

    - update base folder structure by app name folder - add spring boot conf processor - update gradle build according to new generation approach - add spring run method to be able to launch spring - fix test class according to spring standarts

---
+++ src/test/java/org/bat2/vacationworker/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.bat2.vacationworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
