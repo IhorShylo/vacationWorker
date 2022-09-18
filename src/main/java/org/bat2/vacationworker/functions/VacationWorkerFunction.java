c27ef876842bf16843be304128c72c8ce9f97de9
Ihor Shylo <igor_shilo@ukr.net>
2022-09-18 14:12:24 +0300

    -add logging options

--- src/main/java/org/bat2/vacationworker/functions/VacationWorkerFunction.java
+++ src/main/java/org/bat2/vacationworker/functions/VacationWorkerFunction.java
@@ -7,11 +7,15 @@ import com.google.cloud.functions.HttpResponse;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.net.HttpURLConnection;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class VacationWorkerFunction implements HttpFunction {
+    private static final Logger logger = Logger.getLogger(VacationWorkerFunction.class.getName());
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
+
+
         BufferedWriter writer = response.getWriter();
         final BufferedReader reader = request.getReader();
         final String contentType = request.getContentType().orElse("");
@@ -19,6 +23,17 @@ public class VacationWorkerFunction implements HttpFunction {
         final String requestBody = reader.lines().collect(Collectors.joining());
         writer.write("request body: " + System.lineSeparator() + requestBody);
 
+        printLogs(requestBody);
         response.setStatusCode(HttpURLConnection.HTTP_OK);
     }
+
+    private static void printLogs(String body) {
+        System.out.println("I am a log to stdout! Request body:" + body);
+        System.err.println("I am a log to stderr! Request body:" + body);
+
+        logger.info("I am an info log! Request body:" + body);
+        logger.warning("I am a warning log! Request body:" + body);
+    }
+
+
 }
